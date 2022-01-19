0c1c7ae5cd47b5fedafa441bb245cd5d66816237
gabrielprns <gabrielpedrornn@outlook.com>
2022-01-19 00:02:09 -0300

    Project

--- src/Aula2/Principal.java
+++ src/Aula2/Principal.java
@@ -11,7 +11,10 @@ package Aula2;
  */
 public class Principal {
     public static void main(String[] args) {
-        
+        String cor, modelo ;
+        float Ponta ;
+        int carga;
+        boolean Tampada;
     }
     
 }
