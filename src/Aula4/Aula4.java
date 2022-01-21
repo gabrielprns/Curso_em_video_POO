ec028eb1440a831c682ae2607f6ed65b381f4041
gabrielprns <gabrielpedrornn@outlook.com>
2022-01-20 23:55:09 -0300

    Project

---
+++ src/Aula4/Aula4.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aula4;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Aula4 {
+    public static void main(String[] args) {
+        Caneta c = new Caneta();
+        c.setModelo("BIC");
+        c.setPonta(0.9f);
+        c.status(); 
+        
+    }
+    
+}
---
+++ src/Aula4/Caneta.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aula4;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Caneta {
+    private String modelo;
+    private float ponta;
+    
+    public String getModelo(){
+        return this.modelo;
+    }
+    
+    public void setModelo(String m){
+        this.modelo = m;
+    }
+    
+    public float getPonta(){
+        return this.ponta;
+    }
+    
+    public void setPonta(float p){
+        this.ponta= p;
+    }
+    
+    public void status(){
+        System.out.println("Sobre a caneta: ");
+        System.out.println("Modelo: " + this.getModelo());
+        System.out.println("Ponta: " + this.getPonta());
+    
+    }
+    
+}
