60cefae2947fe0419fd6c3a05be2ed88b67bfca7
gabrielprns <gabrielpedrornn@outlook.com>
2022-01-22 00:22:10 -0300

    Project

--- src/Aula5/ContaBanco.java
+++ src/Aula5/ContaBanco.java
@@ -17,11 +17,55 @@ public class ContaBanco {
     private float saldo;
     private boolean status;
     
-    //Métodos
+    //Métodos Especiais e Construtor
     
     public ContaBanco(){
         saldo=0;
         status=false;
     }
+
+    public int getNumConta() {
+        return numConta;
+    }
+
+    public void setNumConta(int numConta) {
+        this.numConta = numConta;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDono() {
+        return dono;
+    }
+
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+    
+    //Métodos
+    
+    
     
 }
