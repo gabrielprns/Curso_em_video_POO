f62ca49b7a9959bb6e776407097879050473b240
gabrielprns <gabrielpedrornn@outlook.com>
2022-01-21 23:49:55 -0300

    Project

---
+++ src/Aula5/ContaBanco.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aula5;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ContaBanco {
+    //Atributos
+    public int numConta;
+    protected String tipo;
+    private String dono;
+    private float saldo;
+    private boolean status;
+    
+    //Métodos
+    
+}
