60a6618c3a2b88abdb6274c925068597ef919f4e
gabrielprns <gabrielpedrornn@outlook.com>
2022-01-23 01:11:52 -0300

    Project

---
+++ src/Aula7/Lutador.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aula7;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Lutador {
+    private String nome, nacionalidade, categoria;
+    private int idade, vitorias, derrotas, empates;
+    private float peso, altura;
+    
+    
+}
