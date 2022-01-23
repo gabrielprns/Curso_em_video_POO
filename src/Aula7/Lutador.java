6ab0c69d0091a56e545947e02fc07513af6dd420
gabrielprns <gabrielpedrornn@outlook.com>
2022-01-23 01:19:08 -0300

    Project

--- src/Aula7/Lutador.java
+++ src/Aula7/Lutador.java
@@ -14,5 +14,113 @@ public class Lutador {
     private int idade, vitorias, derrotas, empates;
     private float peso, altura;
     
+    //Métodos Públicos
+    
+    public void apresentar(){
+    }
+    
+    public void status(){
+    }
+    
+    public void ganharLuta(){
+    }
+    
+    public void perderLuta(){
+    }
+    
+    public void empatarLuta(){
+    
+    }
+    
+    //Método Construtor
+
+    public Lutador(String no, String na, int id, int vi,
+                   int de, int em, float pe, float al){
+        this.nome = no;
+        this.nacionalidade = na;
+        this.idade = id;
+        this.vitorias = vi;
+        this.derrotas = de;
+        this.empates = em;
+        this.peso = pe;
+        this.altura = al;
+    }
+    
+    //Métodos Especiais
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    private void setCategoria() {
+        this.categoria = categoria;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getVitorias() {
+        return vitorias;
+    }
+
+    public void setVitorias(int vitorias) {
+        this.vitorias = vitorias;
+    }
+
+    public int getDerrotas() {
+        return derrotas;
+    }
+
+    public void setDerrotas(int derrotas) {
+        this.derrotas = derrotas;
+    }
+
+    public int getEmpates() {
+        return empates;
+    }
+
+    public void setEmpates(int empates) {
+        this.empates = empates;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+        this.setCategoria();
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+    
+    
     
 }
