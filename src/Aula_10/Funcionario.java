b7c51a829704dd140462b5e9d890c08653e1e487
gabrielprns <gabrielpedrornn@outlook.com>
2022-01-24 22:29:03 -0300

    Project

--- src/Aula_10/Funcionario.java
+++ src/Aula_10/Funcionario.java
@@ -10,5 +10,29 @@ package Aula_10;
  * @author Usuario
  */
 public class Funcionario {
+    private String setor;
+    private boolean trabalhando;
+    
+    public void mudaTrabalho(){
+    
+    }
+
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+
+    public boolean isTrabalhando() {
+        return trabalhando;
+    }
+
+    public void setTrabalhando(boolean trabalhando) {
+        this.trabalhando = trabalhando;
+    }
+    
+    
     
 }
