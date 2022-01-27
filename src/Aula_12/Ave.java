c451f1303cc1246861da581cc3a0a96b35c47ec9
gabrielprns <gabrielpedrornn@outlook.com>
2022-01-26 23:49:33 -0300

    Project

---
+++ src/Aula_12/Ave.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aula_12;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Ave extends Animal {
+    
+    public String corPena;
+
+    @Override
+    public void locomover() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void alimentar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void emitirSom() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getCorPena() {
+        return corPena;
+    }
+
+    public void setCorPena(String corPena) {
+        this.corPena = corPena;
+    }
+    
+    
+    
+}
