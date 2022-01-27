065001eb2ecb5973806032d30f54ef4ea2556ca8
gabrielprns <gabrielpedrornn@outlook.com>
2022-01-27 01:47:29 -0300

    Project

--- src/Aula_12_13/Cachorro.java
+++ src/Aula_12_13/Cachorro.java
@@ -17,14 +17,32 @@ public class Cachorro extends Mamifero {
     }
     
     public void reagir(String frase){
+        if (frase.equals("Toma Comida")||frase.equals("Olá")){
+            System.out.println("Abanar e latir");
+        }else{
+            System.out.println("Rosnar");
+        }
     
     }
     
     public void reagir(int hora, int min){
+        if (hora<12){
+            System.out.println("Abanar");
+        }else if(hora>=18){
+            System.out.println("Ignorar");
+        }else{
+            System.out.println("Abanar e Latir");
+        }
+        
     
     }
     
     public void reagir(boolean Dono){
+        if(Dono){
+            System.out.println("Abanar");
+        }else{
+            System.out.println("Rosnar e Latir");
+        }
     
     }
 
