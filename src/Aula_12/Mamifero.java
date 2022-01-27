c4ebad496be914db9011a32d9738577d58626347
gabrielprns <gabrielpedrornn@outlook.com>
2022-01-26 23:44:20 -0300

    Project

---
+++ src/Aula_12/Mamifero.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aula_12;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Mamifero extends Animal {
+    
+    public String corPelo;
+
+    @Override
+    public void locomover() {
+        System.out.println("Correndo");
+    }
+
+    @Override
+    public void alimentar() {
+        System.out.println("Mamando");
+    }
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Som de mamífero");
+    }
+    
+}
