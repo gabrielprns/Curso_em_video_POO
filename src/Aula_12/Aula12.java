62f0b676ed266f522af98608c7e3ca08dd9a6a65
gabrielprns <gabrielpedrornn@outlook.com>
2022-01-27 00:20:37 -0300

    Project

---
+++ src/Aula_12/Aula12.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aula_12;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Aula12 {
+    public static void main(String[] args) {
+        Mamifero m = new Mamifero();
+        m.alimentar();
+        Cachorro c = new Cachorro();
+        c.emitirSom();
+        
+    }
+    
+}
