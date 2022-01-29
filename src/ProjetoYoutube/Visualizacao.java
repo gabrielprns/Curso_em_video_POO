c4ba038ac3b75a4f31a44a1e1ebc338aa3e22959
gabrielprns <gabrielpedrornn@outlook.com>
2022-01-28 23:56:06 -0300

    Project

--- src/ProjetoYoutube/Visualizacao.java
+++ src/ProjetoYoutube/Visualizacao.java
@@ -16,6 +16,7 @@ public class Visualizacao {
     public Visualizacao(Gafanhoto espectador, Video filme) {
         this.espectador = espectador;
         this.filme = filme;
+        this.espectador.setTotAssistido(this.espectador.getTotAssistido()+1);
     }
 
     public Gafanhoto getEspectador() {
